afc64aa5a5372ed5bd17488b7c41e5b82cde16a7
yuseogi0218 <yuseogi0218@gmail.com>
2023-04-27 02:12:36 +0900

    fix main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from transformers import pipeline
 
 app = FastAPI()
+
+class Item(BaseModel):
+    chat: str
 
 # koElectra-base-v3
 classifier1 = pipeline(
@@ -18,15 +22,16 @@ classifier2 = pipeline(
 )
 
 
-@app.get("/{chat}")
-async def root(chat: str):
-    labels = classifier1(chat)
+@app.post("/")
+async def root(item: Item):
+    print(item.chat)
+    labels = classifier1(item.chat)
     general_score = labels[0][0]['score']
     others_score = labels[0][1]['score']
     if general_score > others_score:
         return {"message": "일반"}
     else:
-        labels = classifier2(chat)
+        labels = classifier2(item.chat)
         question_score = labels[0][0]['score']
         request_score = labels[0][1]['score']
         if question_score > request_score:
