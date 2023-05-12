0e87b2ac4ebe5469ac39b1c7b98411596eef9b75
yuseogi0218 <yuseogi0218@gmail.com>
2023-05-12 19:42:51 +0900

    FEAT - multiprogramming

--- classifier.py
+++ classifier.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 from transformers import pipeline
 
 # koElectra-base-v3
@@ -16,28 +17,26 @@ classifier2 = pipeline(
 )
 
 
-def classifier(chat_list):
-    result_list = list()
-    for chat in chat_list:
-        labels = classifier1(chat)
-        general_score = labels[0][0]['score']
-        others_score = labels[0][1]['score']
-        if general_score > others_score:
-            result_list.append([chat, "일반"])
+def classifier(chat):
+    message = chat[1]
+    labels = classifier1(message)
+    general_score = labels[0][0]['score']
+    others_score = labels[0][1]['score']
+    if general_score > others_score:
+        return [chat, "일반"]
+    else:
+        labels = classifier2(message)
+        question_score = labels[0][0]['score']
+        request_score = labels[0][1]['score']
+        if question_score > request_score:
+            return [chat, "질문"]
         else:
-            labels = classifier2(chat)
-            question_score = labels[0][0]['score']
-            request_score = labels[0][1]['score']
-            if question_score > request_score:
-                result_list.append([chat, "질문"])
-            else:
-                result_list.append([chat, "요청"])
-    return result_list
+            return [chat, "요청"]
 
 
 def preprocess(data_list):
     message_list = list()
     data_json = json.loads(data_list)
     for chat_data in data_json['list']:
-        message_list.append(chat_data["message"])
-    return message_list
+        message_list.append([chat_data["commentNo"], chat_data["message"]])
+    return message_list, data_json['time']
--- main.py
+++ main.py
@@ -2,17 +2,32 @@ from fastapi import FastAPI, WebSocket
 from starlette.websockets import WebSocketDisconnect
 import json
 import classifier
+from multiprocessing import Pool
 
 app = FastAPI()
+num_cores = 4
+pool = Pool(num_cores)
+
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
+
     await websocket.accept()
     try:
         while True:
             data = await websocket.receive_text()
-            preprocessed_data = classifier.preprocess(data)
-            result_list = classifier.classifier(preprocessed_data)
-            await websocket.send_text(json.dumps(result_list, ensure_ascii=False))
+
+            preprocessed_data, request_time = classifier.preprocess(data)
+
+            result_list = pool.map(classifier.classifier, preprocessed_data)
+            print(result_list)
+
+            # 결과 합치기
+            response_data = {
+                'chat_data': result_list,
+                'request_time': request_time,
+            }
+
+            await websocket.send_text(json.dumps(response_data, ensure_ascii=False))
     except WebSocketDisconnect:
         await websocket.close()
